0090c45f49e372a7b91089d0664a3ac1b6f4095d
Arash <arashmahmoudian@Arashs-MacBook-Pro-2.local>
2018-10-25 15:14:51 +0330

    Elements completed

--- src/Defence.java
+++ src/Defence.java
@@ -1,18 +1,18 @@
-public class Defence {
-    @Override
-    public  void getScore(){
-
+public class Defence extends Element {
+    Defence() {
+        super();
     }
-    @Override
-    public void updateIncome(){
-
+    static {
+        LEVEL_LIMIT = 5;
+        CONST_COEFF = 15;
+        BUILD_COST = 10000;
+        REMOVE_COST = -10000;
+        UPGRADE_COST = 5000;
+        NEEDED_UNITS = 30;
+        NEEDED_UNITS_STEP = 0;
     }
-    @Override
-    public int numberOfPeople(){
-
+    public double defenseLevel() {
+        return this.getLevel() * 0.2;
     }
-    @Override
-    public double getScoreOfPerson(){
 
-    }
 }
--- src/Element.java
+++ src/Element.java
@@ -1,35 +1,55 @@
 public class Element {
     private int level, income, blockId;
     private int dayBuilt;
-    Element(int dayBuilt) {
-        income = 0;
+    static int LEVEL_LIMIT = 3;
+    static int CONST_COEFF = 0;
+    static int BUILD_COST = 0;
+    static int REMOVE_COST = 0;
+    static int UPGRADE_COST = 0;
+
+    static int NEEDED_UNITS = 0;
+    static int NEEDED_UNITS_STEP = 0;
+
+    static int getConstCoeff() { return CONST_COEFF; }
+    static int getBuildCost() { return BUILD_COST; }
+    static int getRemoveCost() { return REMOVE_COST; }
+    public int getUpgradeCost() { return UPGRADE_COST; }
+    static int getLevelLimit() { return LEVEL_LIMIT; }
+    Element() {
+        this.setIncome(0);
         this.setLevel(1);
-        this.dayBuilt = dayBuilt;
+        this.dayBuilt = Main.getDay();
     }
     public void setIncome(int newIncome) {
         this.income = newIncome;
     }
     public int getIncome() { return this.income; }
-    public void setLevel(int newLevel) {
-        if (newLevel >= 1 && newLevel <= 3) {
+    public int setLevel(int newLevel) {
+        if (newLevel >= 1 && newLevel <= getLevelLimit()) {
             this.income = newLevel;
+            return 0;
+        } else {
+            // error
+            return -1;
         }
     }
     public int getLevel() { return this.level; }
-    public void levelUp() { setLevel(getLevel() + 1); }
+    public int levelUp() { return setLevel(getLevel() + 1); }
     public void setBlockId(int newBlockId) {
         this.blockId = newBlockId;
     }
     public int getBlockId() { return this.blockId; }
     public int getDayBuilt() { return this.dayBuilt; }
     public double score() {
-        return 0;
+        return Math.pow(this.getConstCoeff(), Main.getDay() - getDayBuilt());
     }
-    public void updateIncome() {
+    public double score(double aCoeff) { return 0; }
 
+    public void updateIncome() {
+        setIncome(getIncome() + numberOfPeople() * 100);
     }
     public int numberOfPeople() {
-        return 0;
+        return NEEDED_UNITS_STEP * (this.getLevel() - 1) + NEEDED_UNITS;
     }
     public double getScoreOfPerson() {
         return 1.;
