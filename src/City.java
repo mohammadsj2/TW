ed7ea32e9b1024c154a59bfd5fdcf06e5d839f26
Mohammad.Sj <mohammad.sj2@gmail.com>
2018-10-25 10:00:10 +0330

    create City blueprint

---
+++ src/City.java
@@ -0,0 +1,25 @@
+public enum City {
+    city1,city2;
+    private Block armyBlock;
+    private ArrayList<Block> blocks;
+    private int money=0;
+
+    public double score(){
+
+    }
+    public void updateMoney(){
+
+    }
+    public void addBlock(){
+
+    }
+    public void removeBlock(int blockId){
+
+    }
+    public void upgradeBlock(int blockId){
+
+    }
+    public int getMoney(){
+
+    }
+}
